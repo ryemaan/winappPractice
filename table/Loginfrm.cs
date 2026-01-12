1e6dc63ca4d4ad58271173793bbdb2d7e90cf307
ryemaan <dhdjbzbsdv@gmail.com>
2026-01-12 15:47:39 +0500

    Add files via upload

---
+++ table/Loginfrm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace table
+{
+    public partial class Loginfrm : Form
+    {
+        public Loginfrm()
+        {
+            InitializeComponent();
+        }
+
+        private void loginbtn_Click(object sender, EventArgs e)
+        {
+            if (logintxt.Text.Trim() == " " || passtxt.Text.Trim() == " ")
+            {
+                MessageBox.Show("Введите логин и пароль");
+                return;
+            }
+            if (logintxt.Text.Trim() == "@Login" && passtxt.Text.Trim() == "@Password")
+            {
+                MessageBox.Show("");
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль");
+            }
+        }
+    }
+}
